6ffa0f39a24272d756aa3e04e93bfb4f8094acc3
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2022-06-01 15:14:05 -0500

    adding Arbitrary instances for practice

--- tests/tests.hs
+++ tests/tests.hs
@@ -23,3 +23,46 @@ prop_thereAndBackAgain = forAll charGen
 
 main :: IO ()
 main = quickCheck prop_thereAndBackAgain
+
+-- 14.6 Arbitrary instances
+-- Babby's First Arbitrary
+data Trivial = Trivial deriving (Eq, Show)
+instance Arbitrary Trivial where
+    arbitrary = trivialGen
+
+trivialGen :: Gen Trivial
+trivialGen = return Trivial
+
+sampTrivialGen :: IO [Trivial]
+sampTrivialGen = sample' trivialGen
+
+-- Identity Crisis
+data Identity a = Identity a deriving (Eq, Show)
+instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = identityGen
+
+identityGen :: Arbitrary a => Gen (Identity a)
+identityGen = do
+    a <- arbitrary
+    return (Identity a)
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt = identityGen
+
+-- Arbitrary Products
+data Pair a b = Pair a b deriving (Eq, Show)
+instance (Arbitrary a, Arbitrary b) =>
+         Arbitrary (Pair a b) where
+     arbitrary = pairGen
+
+pairGen :: (Arbitrary a, Arbitrary b)
+        => Gen (Pair a b)
+pairGen = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Pair a b)
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString = pairGen
+
+-- Greater than the sum of its parts
